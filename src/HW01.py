8f8aafddcb8b928628fb94c1b96af7c853085ea7
Kyriacos Savva <ks0074@morningside.edu>
2026-08-25 14:56:11 -0500

    HW01

--- src/HW01.py
+++ src/HW01.py
@@ -1,10 +1,10 @@
 """
-hello.py
+HW01.py
 ====================================
 This is an example file with correct docstring examples
 
-| Author: Seth McNeill
-| Date: 2025 September 07
+| Author: Kyriacos Savva
+| Date: 2026 August 25
 """
 
 class SayHello:
